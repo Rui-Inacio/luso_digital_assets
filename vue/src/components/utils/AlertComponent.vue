becfdbabd3c9eb7d61da1f46e6643558d7f01860
Rui Inácio <rmy_98@hotmail.com>
2023-08-28 01:14:16 +0100

    Create AlertComponent.vue

---
+++ vue/src/components/utils/AlertComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="type" role="" v-if="show">
+    <div style="float: right; margin-top: -6px;">
+      <button type="button" class="btn" @click="close()">X</button>
+    </div>
+    <h3>
+      {{ message }}
+    </h3>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+export default {
+  props: {
+    message: String,
+    show: Boolean,
+    type: String,
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    }
+  }
+};
+</script>
